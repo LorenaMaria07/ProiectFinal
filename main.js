d1a64d50deb7c31df8615d4205ed3cd57956fd05
LorenaMaria07 <lorenamaria_ursei@yahoo.com>
2025-04-03 18:29:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cookieConsent = document.getElementById('cookieConsent');
+    const acceptCookiesButton = document.getElementById('acceptCookies');
+
+
+    if (!localStorage.getItem('cookieAccepted')) {
+        cookieConsent.style.display = 'block';  
+    }
+
+
+    acceptCookiesButton.addEventListener('click', function () {
+        localStorage.setItem('cookieAccepted', 'true'); 
+        cookieConsent.style.display = 'none'; 
+    });
+});
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
